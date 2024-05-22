25db665090babe3b18e2e22146c038a08f0d83c7
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-05-22 18:43:59 -0400

    more testing here

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, LinkTree } from "./components";
+import React from "react";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className="relative z-0 bg-primary">
-                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-                    <Navbar />
-                    <Hero />
-                </div>
-                <About />
-                <Works />
-                <Experience />
-                {/* <Tech /> */}
-                <Feedbacks />
-                <div className="relative z-0">
-                    <Contact />
-                    <StarsCanvas />
-                </div>
-            </div>
-        </BrowserRouter>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
     );
 };
 
