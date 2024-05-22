ac6483c46c95bda5bfd03995dd6907bca26e1f4e
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-05-22 18:02:53 -0400

    testing with full original project

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,14 @@ const App = () => {
             <div className="relative z-0 bg-primary">
                 <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
                     <Navbar />
+                    <Hero />
                 </div>
+                <About />
+                <Works />
+                <Experience />
+                {/* <Tech /> */}
+                <Feedbacks />
                 <div className="relative z-0">
-                    <LinkTree />
                     <Contact />
                     <StarsCanvas />
                 </div>
