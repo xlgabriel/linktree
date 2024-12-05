41568ff9f70fc8349fe2ebce813416d94c913105
xlgabriel <gabriel.jeannot@uao.edu.co>
2024-12-05 10:02:33 -0500

    refactor: new img and urls

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -70,7 +70,7 @@ const Contact = () => {
             htmlContactTemplate: emailContact,
         };
 
-        fetch("https://videface-server-cars-mqx4ohbz2q-ue.a.run.app/send-contact-email", {
+        fetch("https://videface-backend-166917106706.us-east1.run.app/api/v1/cars/inspections/emails/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -101,15 +101,13 @@ const Contact = () => {
     };
 
     return (
-        <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+        <div id="contact" className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
             <motion.div
                 variants={slideIn("left", "tween", 0.2, 1)}
                 className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
             >
                 <p className={styles.sectionSubText}>Get in touch</p>
-                <h3 id="contact" className={styles.sectionHeadText}>
-                    Contact me.
-                </h3>
+                <h3 className={styles.sectionHeadText}>Contact me.</h3>
 
                 <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
                     <label className="flex flex-col">
--- src/components/LinkTree.jsx
+++ src/components/LinkTree.jsx
@@ -1,17 +1,24 @@
+import { Link } from "react-router-dom";
 import { me, phone, email, youtube } from "../assets";
 import { motion } from "framer-motion";
 
 const LinkTree = () => {
     return (
-        <div id="connect" className="relative z-0 min-h-screen flex flex-col items-center">
+        <div id="linktree" className="relative z-0 min-h-screen flex flex-col items-center">
             <div className="flex flex-col items-center mt-32">
                 <div className="w-32 h-32 rounded-full overflow-hidden mb-8 avatar-violet-gradient">
-                    <img src={me} alt="avatar" className="w-full h-full object-cover p-1 rounded-full" />
+                    <motion.img
+                        src={me}
+                        alt="avatar"
+                        className="w-full h-full object-cover p-1 rounded-full"
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ duration: 0.3 }}
+                    />
                 </div>
                 <h1 className="text-white text-4xl font-bold mb-6">Let's connect</h1>
                 <div className="flex flex-col items-center">
                     <a
-                        href="https://drive.google.com/file/d/11yKzszigs_3QRD9OoXTnPiPn12eY0rFy/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1wNSo6dtOsfWeCJu854kqBztZDKI_ZjBZ/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="mb-4 w-64 text-center green-pink-gradient p-[2px] rounded-[10px] shadow-card"
@@ -20,16 +27,14 @@ const LinkTree = () => {
                             <strong>CV</strong>
                         </button>
                     </a>
-                    <a
-                        href="https://xlgabriel.github.io/portfolio/"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    <Link
+                        to="/"
                         className="mb-4 w-64 text-center green-pink-gradient p-[2px] rounded-[10px] shadow-card"
                     >
                         <button className="w-full bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
                             Portfolio
                         </button>
-                    </a>
+                    </Link>
                     <a
                         href="https://www.linkedin.com/in/xlgabriel"
                         target="_blank"
@@ -52,17 +57,19 @@ const LinkTree = () => {
                     </a>
                     <div className="flex flex-row pt-4 gap-9">
                         <a
+                            href="tel:14076692210"
+                            className="mb-4 text-center violet-gradient rounded-full shadow-card hover:scale-110 transition duration-300 ease-in-out transform"
+                        >
+                            <img src={phone} alt="phone" className="w-9 h-9 inline-block" />
+                        </a>
+                        <a
                             href="mailto:gabriel.jeannot.personal@gmail.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
                             className="mb-4 text-center violet-gradient rounded-full shadow-card hover:scale-110 transition duration-300 ease-in-out transform"
                         >
                             <img src={email} alt="email" className="w-9 h-9 inline-block" />
                         </a>
                         <a
                             href="https://youtube.com/@xlgabriel"
-                            target="_blank"
-                            rel="noopener noreferrer"
                             className="mb-4 text-center violet-gradient rounded-full shadow-card hover:scale-110 transition duration-300 ease-in-out transform"
                         >
                             <img src={youtube} alt="email" className="w-9 h-9 inline-block" />
